9a510d0af5ab1f0ef8475f56e0e12dc1a8ca31aa
William Spriggs <Willspri@gmail.com>
2023-10-31 16:10:07 +1300

    changed

--- loops/loops.py
+++ loops/loops.py
@@ -1,14 +1,25 @@
-
+#normal
 for i in range(6):
     x = "* "
     x = x*i
     print(f'{x: ^10}')
 
+#downwards
 for i in range(6):
     x = "* "
     x = x*(6-i)
     print(f'{x: ^10}')
 
+#left
+for i in range(6):
+    x = "* "
+    x = x*i
+    print(f'{x: <10}')
 
+#right
+for i in range(6):
+    x = "* "
+    x = x*i
+    print(f'{x: >10}')
 
 
